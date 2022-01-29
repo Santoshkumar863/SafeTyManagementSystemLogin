0fe91acbae1aa035fa7a4804e6c3635c1ffdf714
santosh kumar <santoshkumarbethi863@gmail.com>
2022-01-29 14:56:28 +0530

    first commit

---
+++ src/main/java/com/booking/Model/User.java
@@ -0,0 +1,53 @@
+package com.booking.Model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Login")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long userId;
+	
+	private String username;
+	private String password;
+	
+
+	public long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	
+	
+
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", username=" + username + ", password=" + password +  "]";
+	}
+
+}
