3f9b4c5e0517eef6e463a9f7a95c2154096995d5
santosh kumar <santoshkumarbethi863@gmail.com>
2022-01-31 18:18:44 +0530

    first commit

--- src/main/java/com/login/Controller/AdminRestController.java
+++ src/main/java/com/login/Controller/AdminRestController.java
@@ -7,10 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.login.Model.Admin;
@@ -18,6 +17,7 @@ import com.login.Service.AdminServiceimpl;
 
 
 @RestController
+@RequestMapping("/Admin")
 public class AdminRestController
 {
 	@Autowired
